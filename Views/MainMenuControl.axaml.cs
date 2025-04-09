a114a176333c2e1dd3cd040af1496ce770fa38fa
Evgeny Kharetski <e.kharetski@gmail.com>
2025-04-09 23:54:17 +0300

    Application - Menu bar UI #3

--- Views/MainMenuControl.axaml.cs
+++ Views/MainMenuControl.axaml.cs
@@ -12,9 +12,69 @@ public partial class MainMenuControl : UserControl
         
     }
     
-    private void SettingsItem_OnClick(object? sender, RoutedEventArgs e)
+    private void OpenSettingsHandler(object? sender, RoutedEventArgs e)
     {
         var settingsWindow = new SettingsWindow();
         settingsWindow.ShowDialog((Window) this.VisualRoot);
+    }
+
+    private void GroupRenameHandler(object? sender, RoutedEventArgs e)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    private void CloseApplicationHandler(object? sender, RoutedEventArgs e)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    private void AddTabLeftHandler(object? sender, RoutedEventArgs e)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    private void AddTabRightHandler(object? sender, RoutedEventArgs e)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    private void OpenApplicationSettingsJsonHandler(object? sender, RoutedEventArgs e)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    private void ImportSettingsHandler(object? sender, RoutedEventArgs e)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    private void ExportSettingsHandler(object? sender, RoutedEventArgs e)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    private void AboutApplicationHandler(object? sender, RoutedEventArgs e)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    private void WikiHandler(object? sender, RoutedEventArgs e)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    private void GitHubHandler(object? sender, RoutedEventArgs e)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    private void ReportBugHandler(object? sender, RoutedEventArgs e)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    private void SendSuggestionsHandler(object? sender, RoutedEventArgs e)
+    {
+        throw new System.NotImplementedException();
     }
 }
