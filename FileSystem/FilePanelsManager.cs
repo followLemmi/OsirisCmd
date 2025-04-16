7c0cd17c261d6cbe00604c87137d2d485edb7c59
Evgeny Kharetski <e.kharetski@gmail.com>
2025-04-17 01:39:07 +0300

    File System - File lists and navigation #9

---
+++ FileSystem/FilePanelsManager.cs
@@ -0,0 +1,28 @@
+﻿namespace FileSystem;
+
+public class FilePanelsManager
+{
+    private Dictionary<string, FilePanel> FilePanels = new();
+
+    public FilePanelsManager()
+    {
+        LoadFilePanels();
+    }
+    
+    private void LoadFilePanels()
+    {
+    }
+
+    public Dictionary<string, FilePanel> GetFilePanels()
+    {
+        return FilePanels;
+    }
+    
+    public string CreateNewFilePanel()
+    {
+        var filePanel = new FilePanel();
+        var id = Guid.NewGuid().ToString();
+        FilePanels.Add(id, filePanel);
+        return id;
+    }
+}
