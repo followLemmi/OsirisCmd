8b41e6cdc7019886bd2f70df8b509f9124297b21
Evgeny Kharetski <e.kharetski@gmail.com>
2025-06-24 00:44:22 +0300

    File Searcher settings

--- OsirisCmd.SearchingEngine/Settings/FileSearcherSettings.cs
+++ OsirisCmd.SearchingEngine/Settings/FileSearcherSettings.cs
@@ -9,16 +9,51 @@ public class FileSearcherSettings : ISettings
     public ObservableCollection<SettingItem> Settings { get; set; } = [
         new()
         {
-            Name = "File Indexing Enabled",
+            Name = "FileIndexingEnabled",
             Value = false
         },
         new()
         {
-            Name = "Excluded Directories",
+            Name = "DirectoriesSettings",
+            Value = new List<SettingItem>()
+            {
+                new()
+                {
+                    Name = "SkipDirectories",
+                    Value = new List<string>()
+                    {
+                        "/proc",
+                        "/sys",
+                        "/run",
+                        "/var/run",
+                        "/var/lock",
+                        "/var/tmp",
+                        "/tmp",
+                        "/var/log",
+                        "/var/spool",
+                        "/var/cache",
+                        "/lost+found",
+                        "/boot",
+                        "/home/user/.cache",
+                        "/home/user/.thumbnails",
+                    }
+                },
+                new()
+                {
+                    Name = "FileNameOnly",
+                    Value = new List<string>()
+                    {
+                        "/dev"
+                    }
+                }
+            }
+        },
+        new()
+        {
+            Name = "ExcludedExtensions",
             Value = new List<string>()
             {
-                "Windows",
-                "Program Files",
+                
             }
         }
     ];
